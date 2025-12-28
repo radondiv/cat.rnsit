19ea60d6d699f039a04e715b472a3887301062d7
radondiv <darkphone2546@gmail.com>
2025-12-28 15:02:14 +0530

    Create page.tsx

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+
+'use client';
+import ProfileForm from '@/components/ProfileForm';
+
+export default function ProfilePage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4 h1-liquid">Profile</h1>
+      <ProfileForm />
+    </div>
+  );
+}
